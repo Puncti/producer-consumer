ec6d2c5e0d97a2ba6d71c3fdc1a8a57449aa81ea
Puncti <nwhitma2@kent.edu>
2022-04-07 03:33:08 -0400

    adding inital files

---
+++ producer.c
@@ -0,0 +1,30 @@
+//producer file
+//Nicholas Whitman
+//19 March 2022
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <semaphore.h>
+
+wait()
+signal()
+
+int main ()
+{
+
+
+}
+
+
+wait(S)
+{
+ while(S<=0); //busy
+ S--;
+}
+
+signal(S)
+{
+ S++;
+}
